ffb4fedd3ba6b8e52d1fe2021ea6d20094c0ae64
mohsen <mohsenjavaher1387@gmail.com>
2024-02-09 18:44:50 +0330

    Added new activities

--- app/src/main/java/com/mohsen/supermath/MainActivity.kt
+++ app/src/main/java/com/mohsen/supermath/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.mohsen.supermath
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        val shapesMenuButton = findViewById<Button>(R.id.shapesButton)
+        shapesMenuButton.setOnClickListener{
+            val Intent = Intent(this,shapesMenuActivity::class.java)
+            startActivity(Intent)
+        }
+
+        /*val aboutMenuButton = findViewById<Button>(R.id.aboutButton)
+        aboutMenuButton.setOnClickListener {
+            val Intent = Intent(this,aboutMenuActivity::class.java)
+            startActivity(Intent)
+        }*/
+    }
 }
---
+++ app/src/main/java/com/mohsen/supermath/aboutMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.mohsen.supermath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class aboutMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_menu)
+    }
+}

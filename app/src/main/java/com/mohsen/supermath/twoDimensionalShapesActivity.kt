1ed8568d0a5a5af7e81e9529a08c18d25ee0bc44
mohsen <mohsenjavaher1387@gmail.com>
2024-02-10 22:50:33 +0330

    Added shapes menu

--- app/src/main/java/com/mohsen/supermath/twoDimensionalShapesActivity.kt
+++ app/src/main/java/com/mohsen/supermath/twoDimensionalShapesActivity.kt
@@ -3,11 +3,9 @@ package com.mohsen.supermath
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class shapesMenuActivity : AppCompatActivity() {
+class twoDimensionalShapesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shapes_menu)
+        setContentView(R.layout.activity_two_dimensional_shapes)
     }
-
-
 }
